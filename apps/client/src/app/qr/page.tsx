71c9bab901ebb7b96529a0ded3e364f466f3dcd4
Vera Deeva <verasdee@gmail.com>
2024-06-18 17:52:51 +0700

    fix QR user page after mentor comments

--- apps/client/src/app/qr/page.tsx
+++ apps/client/src/app/qr/page.tsx
@@ -1,15 +1,16 @@
+'use-client'
+
 import QRCode from "react-qr-code";
 
-export default function Index({ params }: {
-    params: {id: string}
-}) {
+export default function Id() {
+    const UserId = '12345';
     return <div className='m-10'>
         <h1 className='text-center mb-8' >My QR-code </h1>
         <div>
             <QRCode
                 size={256}
-                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                value={params.id}
+                className='h-auto max-w-full w-full'
+                value={UserId}
                 viewBox={`0 0 256 256`}
             />
         </div>
