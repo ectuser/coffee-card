53d608886de4c65167f3cea09abd8dbeed3f63c8
Vera Deeva <verasdee@gmail.com>
2024-06-19 19:01:41 +0700

    fix two more corrections in QR user page

--- apps/client/src/app/qr/page.tsx
+++ apps/client/src/app/qr/page.tsx
@@ -2,15 +2,15 @@
 
 import QRCode from "react-qr-code";
 
-export default function Id() {
-    const UserId = '12345';
-    return <div className='m-10'>
+export default function setId() {
+    const userId = '12345';
+    return <div className='p-10'>
         <h1 className='text-center mb-8' >My QR-code </h1>
         <div>
             <QRCode
                 size={256}
                 className='h-auto max-w-full w-full'
-                value={UserId}
+                value={userId}
                 viewBox={`0 0 256 256`}
             />
         </div>
